9577fded2a4766a5941503a3367b24fa42cae1fd
sagarDesai45 <sagar.desai@grovetech.in>
2024-07-11 12:41:24 +0530

    Delete assignment1 directory

--- assignment1/src/main/java/com/example/assignment/exception/ProductNotFound.java
+++
@@ -1,8 +0,0 @@
-package com.example.assignment.exception;
-
-public class ProductNotFound extends RuntimeException {
-    
-	public ProductNotFound(String message) {
-        super(message);
-    }
-}
